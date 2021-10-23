7959537dd447fb9278b417fea37864cdfc93d899
Welfar <jdwelfar91@gmail.com>
2021-10-23 13:51:10 -0500

    Added social sharing buttons

--- ubirocket/src/views/NextLaunches.js
+++ ubirocket/src/views/NextLaunches.js
@@ -1,22 +1,44 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-import Upcoming from "../components/Upcoming"
+import Upcoming from "../components/Upcoming";
+import {
+	FacebookShareButton,
+	WhatsappShareButton,
+	WhatsappIcon,
+	FacebookIcon,
+} from "react-share";
 
-import "../styles/Upcoming.css"
+import "../styles/Upcoming.css";
 
 function NextLaunches() {
-  return(
-    <>
-      <div>
-        <Upcoming />
-      </div>
-      <div>
-        <Link to="/" className="link-navigation">
-          Countdown
-        </Link>
-      </div>
-    </>
-  )
+	const shareUrl = "http://localhost:3000/UpcomingLaunches";
+
+	return (
+		<>
+			<div className="container-nextLaunch">
+				<Upcoming />
+			</div>
+			<div className="link-container">
+				<>
+					<Link to="/" className="link-navigation">
+						Countdown
+					</Link>
+				</>
+				<>
+					<Link to="/FavoriteLunches" className="link-navigation">
+						Favorites
+					</Link>
+				</>
+			</div>
+			<p>Share the launches:</p>
+			<FacebookShareButton url={shareUrl}>
+				<FacebookIcon size={40} round={true} />
+			</FacebookShareButton>
+			<WhatsappShareButton url={shareUrl}>
+				<WhatsappIcon size={40} round={true} />
+			</WhatsappShareButton>
+		</>
+	);
 }
 
-export default NextLaunches
+export default NextLaunches;
