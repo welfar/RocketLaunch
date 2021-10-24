70f49e53facaf5b24b33fb353522f930b6f3205c
Welfar <jdwelfar91@gmail.com>
2021-10-24 10:51:27 -0500

    Updates with path to favorites view

--- ubirocket/src/App.js
+++ ubirocket/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Countdown from "./views/Countdown";
 import NextLaunches from "./views/NextLaunches";
+import Favorites from "./views/Favorites";
 import NotFound from "./views/NotFound";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={Countdown} />
 				<Route exact path="/UpcomingLaunches" component={NextLaunches} />
+				<Route exact path="/FavoriteLunches" component={Favorites} />
 				<Route exact path="*" component={NotFound} />
 			</Switch>
 		</Router>
