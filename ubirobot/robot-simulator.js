fb2be345e970002102c57350d27b3ea63b8592da
Welfar <jdwelfar91@gmail.com>
2021-10-25 13:20:46 -0500

    The functionality is created for the displacement of the robot

--- ubirobot/robot-simulator.js
+++ ubirobot/robot-simulator.js
@@ -1,48 +1,124 @@
-
 export class InvalidInputError extends Error {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
-  }
+	constructor(message) {
+		super();
+		this.message = message;
+	}
 }
 
+const directions = ["north", "east", "south", "west"];
+
 export class Robot {
-  orient() {
-    throw new Error("Remove this statement and implement this function");
-  }
+	orient(position) {
+		if (!directions.includes(position)) {
+			throw new InvalidInputError("wrong error");
+		} else {
+			this.location = position;
+		}
+	}
 
-  get bearing() {
-    throw new Error("Remove this statement and implement this function");
-  }
+	get bearing() {
+		return this.location;
+	}
 
-  get coordinates() {
-    throw new Error("Remove this statement and implement this function");
-  }
+	get coordinates() {
+		return [this.coordinate_x, this.coordinate_y];
+	}
+	turnRight() {
+		switch (this.location) {
+			case "north":
+				this.location = "east";
+				[this.coordinate_x + 1, this.coordinate_y];
+				break;
+			case "east":
+				this.location = "south";
+				[this.coordinate_x, this.coordinate_y - 1];
+				break;
+			case "south":
+				this.location = "west";
+				[this.coordinate_x - 1, this.coordinate_y];
+				break;
+			case "west":
+				this.location = "north";
+				[this.coordinate_x, this.coordinate_y + 1];
+				break;
+		}
+	}
+	turnLeft() {
+		switch (this.location) {
+			case "north":
+				this.location = "west";
+				[this.coordinate_x - 1, this.coordinate_y];
+				break;
+			case "west":
+				this.location = "south";
+				[this.coordinate_x, this.coordinate_y - 1];
+				break;
+			case "south":
+				this.location = "east";
+				[this.coordinate_x + 1, this.coordinate_y];
+				break;
+			case "east":
+				this.location = "north";
+				[this.coordinate_x, this.coordinate_y + 1];
+				break;
+		}
+	}
 
-  turnRight() {
-    throw new Error("Remove this statement and implement this function");
-  }
+	at(x, y) {
+		[(this.coordinate_x = x), (this.coordinate_y = y)];
+	}
 
-  turnLeft() {
-    throw new Error("Remove this statement and implement this function");
-  }
+	advance() {
+		switch (this.location) {
+			case "north":
+				[this.coordinate_x, (this.coordinate_y = this.coordinate_y + 1)];
+				break;
+			case "east":
+				[(this.coordinate_x = this.coordinate_x + 1), this.coordinate_y];
+				break;
+			case "south":
+				[this.coordinate_x, (this.coordinate_y = this.coordinate_y - 1)];
+				break;
+			case "west":
+				[(this.coordinate_x = this.coordinate_x - 1), this.coordinate_y];
+				break;
+		}
+	}
 
-  at() {
-    throw new Error("Remove this statement and implement this function");
-  }
-
-  advance() {
-    throw new Error("Remove this statement and implement this function");
-  }
-
-  instructions() {
-    throw new Error("Remove this statement and implement this function");
-  }
-
-  place() {
-    throw new Error("Remove this statement and implement this function");
-  }
-
-  evaluate() {
-    throw new Error("Remove this statement and implement this function");
-  }
+	static instructions(instructions) {
+		const array = [];
+		Array.from(instructions).forEach((instruction) => {
+			switch (instruction) {
+				case "R":
+					array.push("turnRight");
+					break;
+				case "A":
+					array.push("advance");
+					break;
+				case "L":
+					array.push("turnLeft");
+					break;
+			}
+		});
+		return array;
+	}
+	place({ x, y, direction }) {
+		[(this.coordinate_x = x), (this.coordinate_y = y)];
+		this.location = direction;
+	}
+	evaluate(instructions) {
+		Array.from(instructions).forEach((instruction) => {
+			switch (instruction) {
+				case "R":
+					this.turnRight();
+					break;
+				case "A":
+					this.advance();
+					break;
+				case "L":
+					this.turnLeft();
+					break;
+			}
+		});
+	}
 }
